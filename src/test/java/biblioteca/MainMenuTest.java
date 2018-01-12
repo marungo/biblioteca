91cb1ffe4db8329b87cba50fb7c66dbab504b1cc
Jackie Robinson <jrobinso@thoughtworks.com>
2018-01-12 14:19:26 -0600

    Jackie and Ethan created User Input class and test

--- src/test/java/biblioteca/MainMenuTest.java
+++ src/test/java/biblioteca/MainMenuTest.java
@@ -51,4 +51,6 @@ public class MainMenuTest {
         verify(biblioteca).run();
     }
 
+
+
 }
---
+++ src/test/java/biblioteca/UserInputTest.java
@@ -0,0 +1,9 @@
+package biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserInputTest {
+
+}
